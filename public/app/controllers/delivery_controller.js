e1ab513325f1404f67379b97695fc8674a058643
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-06-09 18:30:52 -0300

    Added - delivery controller using google maps API

--- public/app/controllers/delivery_controller.js
+++ public/app/controllers/delivery_controller.js
@@ -1,55 +1,75 @@
 mycontrollers.controller('DeliveryController', function($scope){
 
-    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1cr5VbsjUqqe3wELND6gpbWC6LEhsNcQ"
+    $scope.myAddress = {};
+    $scope.map = {};
+    $scope.uluru = {lat: -34.767074, lng: -58.219502}; //Supermercado Dia - 138 y Av. 14 , Berazategui
+    $scope.myMap = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: $scope.uluru
+    });
 
-    $scope.initialize = function () {
-        var map;
-        function initMap() {
-            var directionsService = new google.maps.DirectionsService();
-            var directionsDisplay = new google.maps.DirectionsRenderer();
+    $scope.supermarketMarker = new google.maps.Marker({
+        position: $scope.uluru,
+        map: $scope.myMap
+    });
 
-            var unqui = {
-                lat: -34.70637,
-                lng: -58.2772431
-            };
-            var addressLucas = {
-                lat: -34.783098,
-                lng: -58.216737
-            };
+    $scope.userMarker = new google.maps.Marker({
+        position: $scope.uluru,
+        map: $scope.myMap
+    });
 
-            var request = {
-                origin: addressLucas,
-                destination: unqui,
-                travelMode: google.maps.TravelMode.DRIVING
-            };
+    $scope.searchAddress = function () {
 
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: unqui,
-                zoom: 11
-            });
+        var geocoder = new google.maps.Geocoder();
+        var resultsMap = $scope.myMap;
 
-            var marker = new google.maps.Marker({
-                position: unqui,
-                map: map,
-                title: 'Universidad Nacional de Quilmes'
-            });
-            var marker = new google.maps.Marker({
-                position: addressLucas,
-                map: map,
-                title: 'Casa de Sandi'
-            });
+        geocoder.geocode(
+            {'address': $scope.myAddress},
 
-            directionsService.route(request, function(response, status) {
+            function (results, status) {
+
+                if (status === 'OK') {
+                    resultsMap.setCenter(results[0].geometry.location);
+
+                    $scope.userMarker.setMap(null);
+
+                    $scope.userMarker = new google.maps.Marker({
+                        map: resultsMap,
+                        position: results[0].geometry.location
+                    });
+
+                    $scope.generateRoute();
+
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            }
+        );
+    };
+
+    $scope.generateRoute = function () {
+
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+
+        directionsDisplay.setMap($scope.myMap);
+
+        directionsService.route( {
+                origin: $scope.supermarketMarker.position,
+                destination: $scope.userMarker.position,
+                travelMode: 'DRIVING'
+            },
+
+            function(response, status) {
                 if (status == google.maps.DirectionsStatus.OK) {
+                    console.log("Estoy entrando al OK");
+
                     directionsDisplay.setDirections(response);
-                    directionsDisplay.setMap(map);
                 } else {
                     alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
                 }
-            });
-        }
-    };
+        });
 
-    $scope.initialize();
+    };
 
 });
