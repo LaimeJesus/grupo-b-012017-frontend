588b897407bd7cefb2cd227791c9e4830e604d0b
jesus <laimejesu@gmail.com>
2017-06-13 13:46:08 -0300

    added notification for ready to buy and waiting

--- public/app/controllers/main_controller.js
+++ public/app/controllers/main_controller.js
@@ -1,6 +1,12 @@
-mycontrollers.controller('MainController', function ($scope, $location, UserService, spinnerService, ShopService, $q) {
+mycontrollers.controller('MainController', function ($scope, $location, UserService, spinnerService, ShopService, $q, $timeout) {
     $scope.user = UserService.getUser();
     $scope.logged = UserService.islogged();
+
+    $scope.shopping = {};
+    $scope.shopping.waiting = false;
+    $scope.shopping.seconds = 0;
+    $scope.shopping.ready = false;
+    $scope.shopping.listId = false;
 
     $scope.someoneLogged = function () {
         return UserService.islogged();
@@ -30,29 +36,50 @@ mycontrollers.controller('MainController', function ($scope, $location, UserServ
 
     $scope.callbackPuedeComprar = function(){
       console.log("puede comprar");
-      // $scope.shopping.canBuy = true;
       ShopService.setCanBuy(true);
-      // $scope.shopping.listId = null;
+      $scope.showReadyAlert();
       spinnerService.hide('generalSpinner');
-    }
+    };
 
     $scope.errorPuedeComprar = function(){
       console.log("no puede comprar");
-      // $scope.shopping.canBuy = false;
-      // $scope.shopping.listId = null;
       ShopService.resetTimer();
       spinnerService.hide('generalSpinner');
+    };
+
+    $scope.showWaitingAlert = function(){
+      // $scope.shopping.waiting = true;
+      $('#waiting').show();
+      $timeout(function () {
+        // $scope.shopping.waiting = false;
+        $('#waiting').hide();
+      }, 3000);
+    };
+
+    $scope.showReadyAlert = function(){
+      // $scope.shopping.ready = true;
+     $('#ready').show();
+      $timeout(function () {
+        // $scope.shopping.ready = false;
+         $('#ready').hide();
+      }, 3000);
     }
 
-    $scope.$on('start', function(event, ms){
-      var seconds = Math.ceil(ms / 1000);
+    $scope.$on('start', function(event, json){
+
+      var seconds = Math.ceil(json.ms / 1000);
+      $scope.shopping.seconds = seconds;
+      $scope.shopping.listId = json.listId;
+
+      $scope.showWaitingAlert();
+
       console.log("waiting" + seconds);
       var defer = $q.defer();
       defer.promise.then($scope.callbackPuedeComprar, $scope.errorPuedeComprar);
       var timer = setInterval(function() {
         $scope.$apply();
-        console.log(seconds);
-        console.log("..");
+        // console.log(seconds);
+        // console.log("..");
         if (seconds === 0) {
               clearInterval(timer);
               defer.resolve();
