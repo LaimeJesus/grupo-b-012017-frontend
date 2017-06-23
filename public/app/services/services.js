032a22961f29fdc7121aba0cf6e929d889858ae0
jesus <laimejesu@gmail.com>
2017-06-23 17:20:04 -0300

    using heroku

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,3 +1,3 @@
 var myservices = angular.module('aloloco-app.myservices', []);
 
-myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/');
+myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/rest/');
