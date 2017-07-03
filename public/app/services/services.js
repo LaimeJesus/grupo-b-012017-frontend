3d42a0d09860301504fb52c7445eb2c107198e5a
jesus <laimejesu@gmail.com>
2017-07-03 18:28:28 -0300

    changed url base to backup

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,7 +1,10 @@
 var myservices = angular.module('aloloco-app.myservices', []);
 
 // PRODUCCION
-myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/rest/');
+// myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/rest/');
+
+// BACKUP
+myservices.constant('urlbase','https://grupo-b-012017-backup.herokuapp.com/rest/');
 
 // Desarrollo
  // myservices.constant('urlbase', 'http://localhost:8080/rest/');
