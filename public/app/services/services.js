0114a38c1c590a4857562d05b245fba7688c522a
jesus <laimejesu@gmail.com>
2017-07-03 17:54:20 -0300

    changed url base for heroku

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,7 +1,7 @@
 var myservices = angular.module('aloloco-app.myservices', []);
 
 // PRODUCCION
-// myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/rest/');
+myservices.constant('urlbase', 'https://grupo-b-012017.herokuapp.com/rest/');
 
 // Desarrollo
- myservices.constant('urlbase', 'http://localhost:8080/rest/');
+ // myservices.constant('urlbase', 'http://localhost:8080/rest/');
